77010f3f5859ff2ff0259c4695655fb9ab987e8a
cptduck <mrdiamondfacehf@gmail.com>
2013-09-26 20:21:34 -0700

    Fixed an error.

--- Interstellar Fruit Wars/src/com/ken/game/spacefruit/entities/Player.java
+++ Interstellar Fruit Wars/src/com/ken/game/spacefruit/entities/Player.java
@@ -24,13 +24,13 @@ public class Player extends Entity {
 	
 	/** Player Settings */
 	private int MOVE_SPEED = 1;
-	private int currentHealth = 100;
+	private int currentHealth;
 	
 	public Player(int x, int y) {
 		
 		super(x, y, 8, 20);
 		
-		this.currentHealth = 1;
+		this.currentHealth = 100;
 		this.x = x;
 		this.y = y;
 	}
