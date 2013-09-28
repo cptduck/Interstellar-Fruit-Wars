8318b83f43c8708f9a1246739d0a6b0447ba6ebd
cptduck <mrdiamondfacehf@gmail.com>
2013-09-27 17:38:23 -0700

    Revert "Bullets are broke as entity, needs fixing."
    This reverts commit c806d2dab46670ce0a34e1b50a2e446227da4aa9.

--- Interstellar Fruit Wars/src/com/ken/game/spacefruit/entities/Bullet.java
+++ Interstellar Fruit Wars/src/com/ken/game/spacefruit/entities/Bullet.java
@@ -3,7 +3,6 @@ package com.ken.game.spacefruit.entities;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -14,8 +13,6 @@ public class Bullet extends Entity {
 	public static int bulletY;
 	private static int bulletX;
 	
-	/* Actions */
-	private static final int Fire = Input.KEY_SPACE;
 	
 	public Bullet(int x, int y) {
 		
@@ -32,29 +29,11 @@ public class Bullet extends Entity {
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 			
 			g.drawImage(new Image(Strings.Bullet), bulletX, bulletY);
-			
-			if (Player.isFiring) {
-				
-				bulletY--;
-				
-			}
 		
 	}
 
 	@Override
-	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException { 
-		
-		Input input = gc.getInput();
-		
-		if (input.isKeyPressed(Fire)) {
-			
-			Player.playerX = bulletX;
-			Player.playerY = bulletY;
-			Player.isFiring = true;
-			
-		}
-		
-	}
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException { }
 	
 	
 }
