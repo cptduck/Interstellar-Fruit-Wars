c806d2dab46670ce0a34e1b50a2e446227da4aa9
cptduck <mrdiamondfacehf@gmail.com>
2013-09-27 17:10:00 -0700

    Bullets are broke as entity, needs fixing.

--- Interstellar Fruit Wars/src/com/ken/game/spacefruit/entities/Bullet.java
+++ Interstellar Fruit Wars/src/com/ken/game/spacefruit/entities/Bullet.java
@@ -3,6 +3,7 @@ package com.ken.game.spacefruit.entities;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -13,6 +14,8 @@ public class Bullet extends Entity {
 	public static int bulletY;
 	private static int bulletX;
 	
+	/* Actions */
+	private static final int Fire = Input.KEY_SPACE;
 	
 	public Bullet(int x, int y) {
 		
@@ -29,11 +32,29 @@ public class Bullet extends Entity {
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 			
 			g.drawImage(new Image(Strings.Bullet), bulletX, bulletY);
+			
+			if (Player.isFiring) {
+				
+				bulletY--;
+				
+			}
 		
 	}
 
 	@Override
-	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException { }
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException { 
+		
+		Input input = gc.getInput();
+		
+		if (input.isKeyPressed(Fire)) {
+			
+			Player.playerX = bulletX;
+			Player.playerY = bulletY;
+			Player.isFiring = true;
+			
+		}
+		
+	}
 	
 	
 }
